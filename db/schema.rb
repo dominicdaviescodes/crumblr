903234c51c5f0eaf1f569ff73493237e48361224
dominicdaviescodes <codewithdom@gmail.com>
2020-03-04 23:10:15 +0000

    edit post partial to allow images in posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_212440) do
+ActiveRecord::Schema.define(version: 2020_03_04_223954) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "kind"
   end
 
 end
