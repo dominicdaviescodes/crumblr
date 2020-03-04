5c453a3e9d695ccc5371a79003c85f0162267b78
dominicdaviescodes <codewithdom@gmail.com>
2020-03-04 23:28:46 +0000

    add form field for users to select image post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,6 +59,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :kind, :content)
     end
 end
